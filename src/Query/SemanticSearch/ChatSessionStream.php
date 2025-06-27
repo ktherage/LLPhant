64bbd2a34ef3e857011e24d4aae40b2ae02040f0
Kevin THERAGE <kevin.therage@sensiolabs.com>
2025-06-27 16:57:38 +0200

    feat(httpinterop): Remove direct Guzzle dependency from ChatSessionStream class

--- src/Query/SemanticSearch/ChatSessionStream.php
+++ src/Query/SemanticSearch/ChatSessionStream.php
@@ -2,14 +2,15 @@
 
 namespace LLPhant\Query\SemanticSearch;
 
-use GuzzleHttp\Psr7\StreamDecoratorTrait;
 use Psr\Http\Message\StreamInterface;
 
-class ChatSessionStream implements StreamInterface
+class ChatSessionStream implements \Stringable, StreamInterface
 {
-    use StreamDecoratorTrait;
-
     private string $answer = '';
+
+    public function __construct(private readonly StreamInterface $stream)
+    {
+    }
 
     public function getContents(): string
     {
@@ -33,4 +34,71 @@ class ChatSessionStream implements StreamInterface
     {
         return $this->answer;
     }
+
+    // Decorated methods...
+    public function __toString(): string
+    {
+        return (string) $this->stream;
+    }
+
+    public function close(): void
+    {
+        $this->stream->close();
+    }
+
+    public function detach()
+    {
+        return $this->stream->detach();
+    }
+
+    public function getSize(): ?int
+    {
+        return $this->stream->getSize();
+    }
+
+    public function tell(): int
+    {
+        return $this->stream->tell();
+    }
+
+    public function eof(): bool
+    {
+        return $this->stream->eof();
+    }
+
+    public function isSeekable(): bool
+    {
+        return $this->stream->isSeekable();
+    }
+
+    public function seek(int $offset, int $whence = SEEK_SET): void
+    {
+        $this->stream->seek($offset, $whence);
+    }
+
+    public function rewind(): void
+    {
+        $this->stream->rewind();
+    }
+
+    public function isWritable(): bool
+    {
+        return $this->stream->isWritable();
+    }
+
+    public function write(string $string): int
+    {
+        return $this->stream->write($string);
+    }
+
+    public function isReadable(): bool
+    {
+        return $this->stream->isReadable();
+    }
+
+    public function getMetadata(?string $key = null)
+    {
+        return $this->stream->getMetadata($key);
+    }
+    // end decorated methods...
 }
