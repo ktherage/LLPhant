76898d21f911996478e63c61bc589aa8ebe6ae70
Kevin THERAGE <kevin.therage@sensiolabs.com>
2025-06-27 16:00:43 +0200

    feat(httpinterop): Remove direct Guzzle dependency from LLPhantTypesenseClient class

--- src/Embeddings/VectorStores/Typesense/LLPhantTypesenseClient.php
+++ src/Embeddings/VectorStores/Typesense/LLPhantTypesenseClient.php
@@ -2,59 +2,54 @@
 
 namespace LLPhant\Embeddings\VectorStores\Typesense;
 
-use GuzzleHttp\Client;
-use GuzzleHttp\ClientInterface;
-use GuzzleHttp\Exception\ClientException;
-use GuzzleHttp\Exception\ServerException;
-use GuzzleHttp\RequestOptions;
+use Http\Discovery\Psr17Factory;
+use Http\Discovery\Psr18ClientDiscovery;
+use Psr\Http\Client\ClientExceptionInterface;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\StreamFactoryInterface;
 
 class LLPhantTypesenseClient
 {
     protected readonly ClientInterface $client;
+
+    protected readonly StreamFactoryInterface&RequestFactoryInterface $factory;
+
+    protected readonly string $baseUri;
+
+    protected readonly string $apiKey;
 
     public function __construct(
         ?string $node = null,
         ?string $apiKey = null,
         ?ClientInterface $client = null
     ) {
-        $this->client = $client instanceof ClientInterface ? $client : $this->createClient($node, $apiKey);
-    }
-
-    private function createClient(?string $node, ?string $apiKey): ClientInterface
-    {
-        if ($node === null) {
-            $node = getenv('TYPESENSE_NODE') ?: 'http://localhost:8108';
+        if ($node === null && is_string(getenv('TYPESENSE_NODE'))) {
+            $node = getenv('TYPESENSE_NODE');
         }
-
-        if ($apiKey === null) {
-            $apiKey = getenv('TYPESENSE_API_KEY') ?: throw new \Exception('You have to provide a TYPESENSE_API_KEY env var to connect to Typesense.');
+        $this->baseUri = $node ?? 'http://localhost:8108';
+        if ($apiKey === null && is_string(getenv('TYPESENSE_API_KEY'))) {
+            $apiKey = getenv('TYPESENSE_API_KEY');
         }
-
-        return new Client([
-            'base_uri' => $node,
-            'headers' => [
-                'X-TYPESENSE-API-KEY' => $apiKey,
-                'Content-Type' => 'application/json',
-                'Accept' => 'application/json',
-            ],
-        ]);
+        $this->apiKey = $apiKey ?? throw new \Exception('You have to provide a TYPESENSE_API_KEY env var to connect to Typesense.');
+        $this->client = $client ?? Psr18ClientDiscovery::find();
+        $this->factory = new Psr17Factory;
     }
 
     public function collectionExists(string $name): bool
     {
-        try {
-            $response = $this->client->request('GET', '/collections/'.$name, []);
+        $response = $this->sendRequest('GET', '/collections/'.$name, []);
 
-            $result = \json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR);
-
-            return \array_key_exists('name', $result);
-        } catch (ServerException|ClientException $e) {
-            if ($e->getCode() === 404) {
-                return false;
-            }
-            $response = $e->getResponse();
-            throw new \Exception('Typesense API error: '.$response->getBody()->getContents(), $e->getCode(), $e);
+        $status = $response->getStatusCode();
+        if ($status === 404) {
+            return false;
         }
+        if ($status < 200 || $status >= 300) {
+            throw new \Exception('Typesense API error: '.$response->getBody()->getContents());
+        }
+
+        return \array_key_exists('name', json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR));
     }
 
     public function createCollection(string $name, int $embeddingLength, string $vectorName): void
@@ -94,7 +89,7 @@ class LLPhantTypesenseClient
             ],
         ];
 
-        $this->post('/collections', $payload);
+        $this->sendRequest('POST', '/collections', $payload);
     }
 
     /**
@@ -104,38 +99,38 @@ class LLPhantTypesenseClient
      */
     public function upsert(string $collectionName, array $point): void
     {
-        $this->post('/collections/'.$collectionName.'/documents?action=upsert', $point);
+        $this->sendRequest('POST', '/collections/'.$collectionName.'/documents?action=upsert', $point);
     }
 
     /**
      * @param  array<string, mixed>  $query
      * @return array<string, mixed>
+     *
+     * @throws \JsonException
      */
     public function multiSearch(array $query): array
     {
-        return $this->post('/multi_search', $query);
+        $response = $this->sendRequest('POST', '/multi_search', $query);
+
+        return \json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR);
     }
 
     /**
-     * @param  array<string, mixed>  $payload
-     * @return array<string, mixed>
+     * @param  array<mixed>  $body
      *
-     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @throws ClientExceptionInterface
      * @throws \JsonException
      */
-    private function post(string $path, array $payload): array
+    protected function sendRequest(string $method, string $path, array $body): ResponseInterface
     {
-        $options = [
-            RequestOptions::JSON => $payload,
-        ];
+        $url = sprintf('%s/%s', rtrim($this->baseUri, '/'), ltrim($path, '/'));
 
-        try {
-            $response = $this->client->request('POST', $path, $options);
+        $request = $this->factory->createRequest($method, $url)
+            ->withHeader('Content-Type', 'application/json')
+            ->withHeader('Accept', 'application/json')
+            ->withHeader('X-TYPESENSE-API-KEY', $this->apiKey)
+            ->withBody($this->factory->createStream(json_encode($body, JSON_THROW_ON_ERROR)));
 
-            return \json_decode($response->getBody()->getContents(), true, 512, JSON_THROW_ON_ERROR);
-        } catch (ServerException|ClientException $e) {
-            $response = $e->getResponse();
-            throw new \Exception('Typesense API error: '.$response->getBody()->getContents(), $e->getCode(), $e);
-        }
+        return $this->client->sendRequest($request);
     }
 }
